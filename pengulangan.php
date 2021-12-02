88edf252da088e382b76c398d31b099d4ee09a76
nabilafauziya <94823946+nabilafauziya@users.noreply.github.com>
2021-12-02 10:24:07 +0700

    Add files via upload

---
+++ pengulangan.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Pengulangan</title>
+</head>
+<body>
+    <form action="" method="post">
+
+    <table>
+        <h1>Form Pengulangan</h1>
+
+        <tr >
+            <td>1. Deret Bilangan Ganjil</td>
+        </tr>
+        <tr>
+            <td>2. Segitiga Sama Kaki Terbalik</td>
+        </tr>
+        <tr>
+            <td>3. Deret Bilangan Kelipatan</td>
+        </tr>
+        <tr>
+            <td><label for="pilih">Pilih</label></td>
+            <td>:</td>
+            <td><input type="text" name="pilih"></td>
+        </tr>
+        <tr>
+            <td><label for="angka">Masukkan Angka</label></td>
+            <td>:</td>
+            <td><input type="text" name="angka"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="submit" value="KIRIM"></td>
+        </tr>
+    </table>
+
+
+    </form>
+</body>
+</html>
